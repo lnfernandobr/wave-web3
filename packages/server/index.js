66e01c8cfaa2b5ad13e8fd41127016f0bae59e83
Fernando <lnfernandobr@gmail.com>
2022-04-25 20:37:03 -0400

    * Added html provider to request with express.

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,1 +1,17 @@
-console.warn("Logging server!")
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.use(express.static("public"));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.info(`Server is running on port ${PORT}`);
+});
